ddcd094d3add138b4fc051a2d49f5edec3be4d67
Muslima Nadia Ofori <oforimuslima95@gmail.comgit config --globaal user.email oforimuslima@gmail.com>
2022-04-11 21:25:28 +0000

    print negative and positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main -assigns a random number to int n everytime
+*it executes, and prints it
+*Return: always 0
+*/
+int main(void)
+{
+        int n;
+               [D[D[D[D[D[D
+[C[C[C[C[C[C[C[Csrand(time(0));
+        n = rand() - RAND_MAX / 2;
+          if (n > 0)
+                  printf("%d is positive\n", n);
+           else if (n == 0)
+                   printf("%d is zero\n", n);
+            else if (n < 0)
+                    printf("%d is negative\n", n);
+            return (0);
+}
