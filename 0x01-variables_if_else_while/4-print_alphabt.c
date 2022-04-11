dc3d1e7433bd78048010ee06747e12302ee35541
Muslima Nadia Ofori <oforimuslima95@gmail.com>
2022-04-11 22:48:36 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+* main - print the alphabet in lowercase and uppercase,
+* followed by a new line using putchar function
+* Return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
