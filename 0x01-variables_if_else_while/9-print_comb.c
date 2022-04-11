738f773f4ec922db55e4916764de1c42346ec3be
Muslima Nadia Ofori <oforimuslima95@gmail.com>
2022-04-11 23:26:54 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
-include
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers of
+* base 10
+* Return: 0
+*/
+int main(void)
+{
+	int number = 48;
+
+	while (number < 58)
+	{
+		putchar(number);
+		if (number != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
+
