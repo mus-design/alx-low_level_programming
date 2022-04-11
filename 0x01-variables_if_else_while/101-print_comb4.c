7e04a5a5fee0eae96d0129b85bc9b3e7737ab7e4
Muslima Nadia Ofori <oforimuslima95@gmail.com>
2022-04-11 23:40:44 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,36 @@
-include
+#include <stdio.h>
+
+/**
+* main - prints all dobule digit numbers of
+* numbers
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+	int secondNumber;
+	int thirdNumber;
+
+	for (number = 48; number < 58; number++)
+	{
+		for (secondNumber = 49; secondNumber < 58; secondNumber++)
+		{
+			for (thirdNumber = 50; thirdNumber < 58; thirdNumber++)
+			{
+				if (thirdNumber > secondNumber && secondNumber > number)
+				{
+					putchar(number);
+					putchar(secondNumber);
+					putchar(thirdNumber);
+					if (number != 55 || secondNumber != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
