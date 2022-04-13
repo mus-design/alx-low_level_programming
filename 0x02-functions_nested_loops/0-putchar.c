91f457a3db6bfd6819b2d301b1f0f5734b4d49dd
Muslima Nadia Ofori <oforimuslima95@gmail.comgit config --globaal user.email oforimuslima@gmail.com>
2022-04-13 21:49:48 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Description: write aprogram[D[D[D[D[D[D[D program that prints "_putchar" followed by a new line
+ *Return: Always 0 (Success.)
+ */
+ int main
