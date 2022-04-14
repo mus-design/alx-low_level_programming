40732067e19765b63797c4233a9ae625e6771d15
Muslima Nadia Ofori <oforimuslima95@gmail.comgit config --globaal user.email oforimuslima@gmail.com>
2022-04-14 00:58:51 +0000

    function that computes Absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @number: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int number)
+{
+	if (number >= 0)
+	{
+		return (number);
+	}
+	return (-number);
+}
