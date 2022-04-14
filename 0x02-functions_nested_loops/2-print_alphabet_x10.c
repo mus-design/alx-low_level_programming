27f5c6f159d8759dccf9ac69b7f12379b75e6fc2
Muslima Nadia Ofori <oforimuslima95@gmail.comgit config --globaal user.email oforimuslima@gmail.com>
2022-04-14 00:29:35 +0000

    latest commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
