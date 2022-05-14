685cce2aa2474ad366abd7fd66e538364bcff222
Muslima Nadia Ofori <oforimuslima95@gmail.com>
2022-05-14 22:00:22 +0000

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,51 +2,19 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: is pointer
  * Return: Nothing.
- */
-void print_name_as_is(char *name)
+**/
+void print_name(char *name, void (*f)(char *))
 {
-printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
+if (name == NULL || f == NULL)
 {
-unsigned int i;
-
-printf("Hello, my uppercase name is ");
-i = 0;
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-{
-putchar(name[i] + 'A' - 'a');
+return;
 }
 else
 {
-putchar(name[i]);
+f(name);
 }
-i++;
-}
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_name("Bob", print_name_as_is);
-print_name("Bob Dylan", print_name_uppercase);
-printf("\n");
-return (0);
 }
